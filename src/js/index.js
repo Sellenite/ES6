5ac1b50dfdc7437dbd71961de5efa2d461701ff7
Sellenite <714627528@qq.com>
2018-01-15 20:39:21 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 /*
- * @Author: yuuhei 
- * @Date: 2018-01-11 13:51:20 
- * @Last Modified by:   Sellenite
- * @Last Modified time: 2018-01-15 15:57:04
+ * @Author: yuuhei
+ * @Date: 2018-01-11 13:51:20
+ * @Last Modified by: Sellenite
+ * @Last Modified time: 2018-01-15 19:38:49
  */
 require('./stage1.js');
 console.log('<!--------Above is Latest-------->')
