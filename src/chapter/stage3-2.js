46e830db41686477136277375cc211cfc2ab6346
Sellenite <714627528@qq.com>
2018-12-12 17:44:27 +0800

    super

--- src/chapter/stage3-2.js
+++ src/chapter/stage3-2.js
@@ -4,5 +4,42 @@
  */
 
 {
+    const runSomething = (obj) => {
+        let x = Math.random(),
+            y = Math.random();
 
+        return obj.something(x, y);
+    }
+
+    let num = runSomething({
+        // 递归引用当前函数的必要写法
+        something: function something(x, y) {
+            if (x > y) {
+                return something(y, x);
+            }
+            return y - x;
+        }
+    });
+
+    console.log(num);
+};
+
+{
+    let obj1 = {
+        foo() {
+            console.log('foo obj1');
+        }
+    };
+
+    let obj2 = {
+        foo() {
+            // 可以这样使用super
+            super.foo();
+            console.log('foo obj2');
+        }
+    };
+
+    // 其实就是将obj1的__proto__赋值给obj2
+    Object.setPrototypeOf(obj2, obj1);
+    obj2.foo();
 };
