254ab7ca1bfd45649ea5d2e2e4ecf38bb1c25fe0
Sellenite <714627528@qq.com>
2018-12-13 22:02:36 +0800

    update

--- src/chapter/stage3-2.js
+++ src/chapter/stage3-2.js
@@ -43,3 +43,41 @@
     Object.setPrototypeOf(obj2, obj1);
     obj2.foo();
 };
+
+{
+    // 标签字符串字面量
+    const foo = (strings, ...values) => {
+        console.log(strings);
+        console.log(values);
+    };
+
+    const desc = 'awesome';
+
+    foo`Everything is ${desc}!`;
+};
+
+{
+    // 标签字符串字面量简单应用
+    const fixNum = (strings, ...values) => {
+        // strings ["you should pay ", " to me"]
+        // values [19.9891]
+        return strings.reduce((prev, curr, currIdx) => {
+            console.log(prev);
+            console.log(curr);
+            if (currIdx > 0) {
+                if (typeof values[currIdx - 1] === 'number') {
+                    prev += `$${values[currIdx - 1].toFixed(2)}`
+                }
+                else {
+                    prev += values[currIdx - 1];
+                }
+            }
+            return prev + curr;
+        }, "");
+    };
+
+    const amt = 19.9891;
+    // 每个通过字面量传入的数字都会被toFixed
+    const result = fixNum`you should pay ${amt} to me`;
+    console.log(result);
+};
