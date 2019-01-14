158f98ac961abcdf591fa3f5a10925ac2a8b2dc1
Sellenite <714627528@qq.com>
2019-01-14 09:24:57 +0800

    update dom

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import './main.scss';
-import './chapter/stage1-1.js';
-import './chapter/stage1-2.js';
-import './chapter/stage2-1.js';
-import './chapter/stage2-2.js';
-import './chapter/stage3-2.js';
+import './youDontKnowJS/index.js';
+import './dom/index.js';
 console.log('<!--------Above is Latest-------->');
 console.log('<!--------Below is ASYNC-------->');
---
+++ src/youDontKnowJS/index.js
@@ -0,0 +1,5 @@
+import './stage1-1.js';
+import './stage1-2.js';
+import './stage2-1.js';
+import './stage2-2.js';
+import './stage3-2.js';
