c2ea6b75413012b5d3f5641a20aacb7c19fb81fa
Sellenite <714627528@qq.com>
2019-02-14 17:01:10 +0800

    update security

--- src/js/interview-general/index.js
+++ src/js/interview-general/index.js
@@ -1,5 +1,6 @@
 import './js-base.js';
 import './js-advanced.js';
 import './promise.js';
+import './security.js';
 console.log('<!--------Above is Latest-------->');
 console.log('<!--------Below is ASYNC--------->');
