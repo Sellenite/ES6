23b9a52763895b43d321c670359d24138d71cd46
Sellenite <714627528@qq.com>
2019-02-15 15:53:58 +0800

    update

--- src/js/interview-general/responsive.js
+++ src/js/interview-general/responsive.js
@@ -21,7 +21,6 @@ class Dep {
     // 更新
     notify() {
         this.subs.forEach(sub => {
-            console.log(sub)
             sub.update();
         })
     }
@@ -46,7 +45,7 @@ class Watcher {
         // 获得新值
         this.value = this.obj[this.key];
         // 调用回调
-        this.cb(this.value);
+        this.cb(this.value, this.key);
     }
 }
 
@@ -66,7 +65,7 @@ function defineReactive(obj, key, val) {
         enumerable: true,
         configurable: true,
         get: function reactiveGetter() {
-            console.log(`get ${val}`);
+            console.log(`get ${key}`);
             // 将 Watcher 添加到订阅
             if (Dep.target) {
                 dp.addSub(Dep.target);
@@ -74,7 +73,7 @@ function defineReactive(obj, key, val) {
             return val;
         },
         set: function reactiveSetter(newVal) {
-            console.log(`set ${newVal}`);
+            console.log(`${key} set to ${newVal}`);
             val = newVal;
             // 执行watcher的update方法
             dp.notify();
@@ -88,12 +87,17 @@ observe(data);
 
 let name = data.name;
 
-function update(value) {
-    document.querySelector('#responsive').innerText = value;
+function update(value, from) {
+    let container = document.querySelector('#responsive');
+    let para = document.createElement('p');
+    para.innerText = `attr ${from} set to ${value}`;
+    container.appendChild(para);
 }
 
-// 模拟解析到 `{{name}}` 触发的操作
+// 模拟解析到 `{{name}}` 等触发的操作
 new Watcher(data, 'name', update);
+new Watcher(data.sub.subin, 'c', update);
 
 // update Dom innerText
 data.name = 'satellite';
+data.sub.subin.c = '011';
